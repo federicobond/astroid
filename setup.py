3e49552b7d0d40b12f323f269a9bd4f95ee0815f
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-04-10 16:47:18 +0200

    Move information to setup.py from __pgkinfo__

--- setup.py
+++ setup.py
@@ -15,56 +15,52 @@
 # Licensed under the LGPL: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
 # For details: https://github.com/PyCQA/astroid/blob/master/LICENSE
 
-# pylint: disable=W0404,W0622,W0613
 """Setup script for astroid."""
 import os
 import sys
-import warnings
 from setuptools import find_packages, setup
-from setuptools.command import easy_install  # pylint: disable=unused-import
-from setuptools.command import install_lib  # pylint: disable=unused-import
-
-if sys.version_info.major == 3 and sys.version_info.minor <= 5:
-    warnings.warn(
-        "You will soon need to upgrade to python 3.6 in order to use the latest version of Astroid.",
-        DeprecationWarning,
-    )
 
 real_path = os.path.realpath(__file__)
 astroid_dir = os.path.dirname(real_path)
-pkginfo = os.path.join(astroid_dir, "astroid", "__pkginfo__.py")
 
-with open(pkginfo, "rb") as fobj:
-    exec(compile(fobj.read(), pkginfo, "exec"), locals())
 
+# pylint: disable=redefined-builtin; why license is a builtin anyway?
+mailinglist = "mailto://%s" % "code-quality@python.org"
 with open(os.path.join(astroid_dir, "README.rst")) as fobj:
     long_description = fobj.read()
 
 
 needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
-pytest_runner = ["pytest-runner"] if needs_pytest else []
-
-
-def install():
-    return setup(
-        name="astroid",
-        version=version,
-        license=license,
-        description=description,
-        long_description=long_description,
-        classifiers=classifiers,
-        author=author,
-        author_email=author_email,
-        url=web,
-        python_requires=">=3.6",
-        install_requires=install_requires,
-        extras_require=extras_require,
-        packages=find_packages(exclude=["tests"]) + ["astroid.brain"],
-        setup_requires=pytest_runner,
-        test_suite="test",
-        tests_require=["pytest"],
-    )
-
-
-if __name__ == "__main__":
-    install()
+setup(
+    name="astroid",
+    version="2.6.0",
+    license="LGPL-2.1-or-later",
+    description="An abstract syntax tree for Python with inference support.",
+    long_description=long_description,
+    classifiers=[
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Quality Assurance",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    author="Python Code Quality Authority",
+    author_email="code-quality@python.org",
+    url="https://github.com/PyCQA/astroid",
+    python_requires=">=3.6",
+    install_requires=[
+        "lazy_object_proxy>=1.4.0",
+        "wrapt>=1.11,<1.13",
+        'typed-ast>=1.4.0,<1.5;implementation_name== "cpython" and python_version<"3.8"',
+    ],
+    extras_require={},
+    packages=find_packages(exclude=["tests"]) + ["astroid.brain"],
+    setup_requires=["pytest-runner"] if needs_pytest else [],
+    test_suite="test",
+    tests_require=["pytest"],
+)
